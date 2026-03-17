5f0551ebcae22478c47c59baf20892880ed2c77d
bibokabeijoka <biabiabiabiaa2000@gmail.com>
2026-03-17 11:12:20 -0300

    versao com a rota sigmas e views

--- appejs/routes/index.js
+++ appejs/routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'revoluçãoRussa' });
+  res.render('index', { title: 'Revolução Russa', descricao: 'A Revolução Russa foi um conjunto de eventos políticos e sociais que ocorreram na Rússia em 1917.' });
 });
 
 module.exports = router;
+
+router.get('/sigmas', function(req, res, next) {
+  res.render('sigmas', { title: 'sigmas' });
+});
+
+module.exports = router;
+
